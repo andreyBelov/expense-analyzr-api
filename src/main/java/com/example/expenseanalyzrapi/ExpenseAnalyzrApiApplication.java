9d7653ace350a67faff43ac31986b696a6d96b7c
belov <andrey.beloborodov.job@gmail.com>
2023-12-16 19:18:01 +0400

    init commit

---
+++ src/main/java/com/example/expenseanalyzrapi/ExpenseAnalyzrApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenseanalyzrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseAnalyzrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseAnalyzrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expenseanalyzrapi/ExpenseAnalyzrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseanalyzrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseAnalyzrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
